fa6a14b201b597f2b13d12ee7cfe440f2674daea
Ross Parsons <rparsons779@gmail.com>
2023-06-19 23:09:29 -0700

    added more functionality

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State private var inputDegree: Double = 0
+    @State private var conversion: String = "Fahrenheit"
+    @FocusState private var keyboardFocus: Bool
+    
+    let units: [String] = ["Celsius", "Fahrenheit", "Kelvin"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Degrees", value: $inputDegree, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($keyboardFocus)
+                } header: {
+                    Text("Starting Degrees")
+                }
+                Section {
+                    Picker("Units", selection: $conversion) {
+                        ForEach(units, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }
         }
-        .padding()
+        .toolbar {
+            ToolbarItem(placement: .keyboard) {
+                Button("Done") {
+                    keyboardFocus = false
+                }
+            }
+        }
     }
 }
 
