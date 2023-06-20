77fa7ecdc52b28bc3f784e0e720b9bc9c29f343f
Ross Parsons <rparsons779@gmail.com>
2023-06-19 22:13:58 -0700

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Ross Parsons on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
